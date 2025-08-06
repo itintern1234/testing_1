11151d289538c96ffb04c540f6342761a8e62261
IT Intern <itintern@SRGASIA.COM.MY>
2025-08-06 11:36:31 +0800

    Testing Change

--- lib/main.dart
+++ lib/main.dart
@@ -104,7 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('Testing\nYou have pushed the button this many times:'),
+            const Text('Testing\nTesting Testing\nYou have pushed the button this many times:'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
